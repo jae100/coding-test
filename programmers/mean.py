7588dd5bcc08b9de35c7d854f419eba155910210
jae100 <108312143+jae100@users.noreply.github.com>
2022-10-11 23:13:10 +0900

    프로그래머스 평균구하기

---
+++ programmers/mean.py
@@ -0,0 +1,16 @@
+arr = [1,2,3,4]
+
+def solution1(arr):
+   return (sum(arr)/len(arr))
+
+print(solution1(arr))
+
+def solution2(arr):
+    answer = 0
+
+    for i in arr:
+        answer += i
+
+    return answer / len(arr)
+print(solution2(arr))
+
